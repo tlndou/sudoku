1ba6b724122363e6758c82dfe9fb57a42bf95244
si-mv <simon.haines@multiverse.io>
2022-07-28 15:12:36 +0100

    fix num rows and cols

--- sudoku.js
+++ sudoku.js
@@ -47,8 +47,8 @@ function setGame(){
     }
     
     //board 9x9
-    for (let r = 0; r <= 9; r++){
-        for (let c = 0; c <= 9; c++){
+    for (let r = 0; r < 9; r++){
+        for (let c = 0; c < 9; c++){
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
             if (board[r][c] != "-"){
